10b72260855831a886bd9b0d2a119131a8a20a15
atemion <antoine.hartmann@pm.me>
2026-06-04 17:37:09 +0200

    Build server without logic

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
-package bootdevchirpy
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	s.ListenAndServe()
+}
