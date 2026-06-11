213f649c410442a308ecd4c3fb9e70fba5c008fc
atemion <antoine.hartmann@pm.me>
2026-06-11 19:47:18 +0200

    Added Readiness Endpoint

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", myHandler)
 
 	s := &http.Server{
 		Addr:    ":8080",
@@ -15,4 +16,10 @@ func main() {
 	}
 
 	s.ListenAndServe()
+}
+
+func myHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
