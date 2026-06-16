d9fc11226d14927af5dfe97a35514aab7065bc3e
atemion <antoine.hartmann@pm.me>
2026-06-16 21:26:16 +0200

    Add JSON responses

--- main.go
+++ main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"sync/atomic"
+	"unicode/utf8"
 )
 
 func main() {
 	mux := http.NewServeMux()
 	apiCfg := &apiConfig{}
 
-	// mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("GET /api/healthz", myHandler)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.serverHitsHandler)
 	mux.HandleFunc("POST /admin/reset", apiCfg.serverHitsReset)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpHander)
 
 	s := &http.Server{
 		Addr:    ":8080",
@@ -28,6 +31,56 @@ func myHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+// New endpoint to validate chirp (JSON chapter)
+
+func validateChirpHander(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, "Something went wrong")
+		return
+	}
+
+	if utf8.RuneCountInString(params.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+		return
+	}
+
+	type validResp struct {
+		Valid bool `json:"valid"`
+	}
+	u := validResp{
+		Valid: true,
+	}
+	respondWithJSON(w, 200, u)
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, errorResponse{
+		Error: msg,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(dat)
 }
 
 // Middleware to log server hits
